5fcc2ba4c078cad5484769e7faa32f0f23758bae
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2022-09-20 19:32:51 +0100

    complete day08

--- day08/ex02/MutantStack.hpp
+++ day08/ex02/MutantStack.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <stack>
+#include <iterator>
+
+template <class T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack()
+	{
+	}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator begin(void)
+	{
+		return this->c.begin();
+	}
+	iterator end(void)
+	{
+		return this->c.end();
+	}
+};
